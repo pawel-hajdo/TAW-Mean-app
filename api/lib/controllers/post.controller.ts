d5ebb3bde97bd0c7c9ad54e96645939636e9ccff
Paweł Hajdo <pawel.hajdo001@gmail.com>
2024-04-19 20:14:12 +0200

    added get endpoint for post with specified id

--- api/lib/controllers/post.controller.ts
+++ api/lib/controllers/post.controller.ts
@@ -1,5 +1,6 @@
 import Controller from "../interfaces/controller.interface";
 import {Request, Response, NextFunction, Router} from "express";
+import {parse} from "graphql/language";
 
 let testArr = [4,5,6,3,5,3,7,5,13,5,6,4,3,6,3,6];
 
@@ -12,15 +13,22 @@ class PostController implements Controller {
     }
 
     private initializeRoutes() {
-        this.router.get(`${this.path}/posts`, this.getAll);
-        this.router.post(`${this.path}/post`, this.addData);
+        this.router.get(`${this.path}/posts`, this.getAllPosts);
+        this.router.get(`${this.path}/post/:id`, this.getOnePost);
+        this.router.post(`${this.path}/post`, this.addPost);
     }
 
-    private getAll = async (request: Request, response: Response, next: NextFunction)=> {
+    private getAllPosts = async (request: Request, response: Response, next: NextFunction)=> {
         response.status(200).json(testArr);
     }
 
-    private addData = async (request: Request, response: Response, next: NextFunction)=> {
+    private getOnePost = async (request: Request, response: Response, next: NextFunction)=> {
+        const { id } = request.params;
+        const postId: number = parseInt(id);
+        response.status(200).json(testArr[postId]);
+    }
+
+    private addPost = async (request: Request, response: Response, next: NextFunction)=> {
         const newPost = request.body;
         testArr.push(newPost);
 
