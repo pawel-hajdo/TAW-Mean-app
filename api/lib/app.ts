1bdf9a55405180d837e393971f79e1e888daac32
Paweł Hajdo <pawel.hajdo001@gmail.com>
2024-04-21 21:48:50 +0200

    added nodemon created connection to database

--- api/lib/app.ts
+++ api/lib/app.ts
@@ -3,7 +3,7 @@ import { config } from './config'
 import Controller from "./interfaces/controller.interface";
 import bodyParser from "body-parser";
 import morgan from "morgan";
-
+import mongoose from 'mongoose'
 class App {
     public app: express.Application;
 
@@ -11,6 +11,7 @@ class App {
         this.app = express();
         this.initializeMiddlewares();
         this.initializeControllers(controllers);
+        this.connectToDatabase();
     }
 
     private initializeControllers(controllers: Controller[]):void{
@@ -23,6 +24,36 @@ class App {
         this.app.use(bodyParser.json());
         this.app.use(morgan('dev'));
     }
+
+    private async connectToDatabase(): Promise<void>{
+        try{
+            await  mongoose.connect(config.databaseUrl);
+            console.log(`connected to database at url ${config.databaseUrl}`);
+        }catch (e) {
+            console.error(`error connecting to DB`,e)
+        }
+        mongoose.connection.on('error',(e)=>{
+            console.error(`error connecting to DB`,e);
+        });
+
+        mongoose.connection.on('disconnected',()=>{
+            console.log(`Disconnected from DB`);
+        })
+
+        process.on('SIGINT',async ()=>{
+            await mongoose.connection.close();
+            console.log(`MongoDB connection closed by app termination`)
+            process.exit(0);
+        })
+
+        process.on('SIGTERM',async ()=>{
+            await mongoose.connection.close();
+            console.log(`MongoDB connection closed by app termination`)
+            process.exit(0);
+        })
+
+    }
+
     public listen(): void {
         this.app.listen(config.port, () => {
             console.log(`App listening on the port ${config.port}`);
--- api/lib/config.ts
+++ api/lib/config.ts
@@ -1,3 +1,4 @@
 export const config = {
-    port: process.env.PORT || 3100
+    port: process.env.PORT || 3100,
+    databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://twwai:KTp5wYwutrLHPLT@cluster0.ooees.mongodb.net/IoT?retryWrites=true&w=majority'
 };
