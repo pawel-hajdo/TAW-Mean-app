951a4636671b01bbe1cbe0fb6327375a33fe9400
Paweł Hajdo <pawel.hajdo001@gmail.com>
2024-04-25 21:08:54 +0200

    changed method in post controller to use dataservice instead of testarray

--- api/lib/controllers/post.controller.ts
+++ api/lib/controllers/post.controller.ts
@@ -1,13 +1,12 @@
 import Controller from "../interfaces/controller.interface";
 import {Request, Response, NextFunction, Router} from "express";
-import {parse} from "graphql/language";
 import {checkPostCount} from "../middlewares/checkPostCount.middleware";
-
-let testArr = [4,5,6,3,5,3,7,5,13,5,6,4,3,6,3,6];
+import DataService from "../modules/services/data.service";
 
 class PostController implements Controller {
     public path = '/api';
     public router = Router();
+    private dataService = new DataService();
 
     constructor() {
         this.initializeRoutes();
@@ -15,53 +14,76 @@ class PostController implements Controller {
 
     private initializeRoutes() {
         this.router.get(`${this.path}/posts`, this.getAllPosts);
-        this.router.get(`${this.path}/post/:id`, this.getOnePost);
+        this.router.get(`${this.path}/post/:id`, this.getPostById);
 
         this.router.post(`${this.path}/post`, this.addPost);
         this.router.post(`${this.path}/post/:num`, checkPostCount, this.getXPosts);
 
         this.router.delete(`${this.path}/posts`, this.deleteAllPosts);
-        this.router.delete(`${this.path}/post/:id`, this.deleteOnePost);
+        this.router.delete(`${this.path}/post/:id`, this.deletePostById);
     }
 
     private getAllPosts = async (request: Request, response: Response, next: NextFunction)=> {
-        response.status(200).json(testArr);
+        try {
+            const allData = await this.dataService.getAllPosts();
+            response.status(200).json(allData);
+        } catch (error) {
+            console.error('Error occurred while retrieving all posts:', error);
+            response.status(500).json({ error: 'Internal server error' });
+        }
     }
 
-    private getOnePost = async (request: Request, response: Response, next: NextFunction)=> {
+    private getPostById = async (request: Request, response: Response, next: NextFunction)=> {
         const { id } = request.params;
-        const postId: number = parseInt(id);
-        response.status(200).json(testArr[postId]);
+        const allData = await this.dataService.query({_id: id});
+        response.status(200).json(allData);
     }
 
     private addPost = async (request: Request, response: Response, next: NextFunction)=> {
-        const newPost = request.body;
-        testArr.push(newPost);
+        const {title, text, image} = request.body;
 
-        response.status(200);
+        const readingData = {
+            title,
+            text,
+            image
+        };
+
+        try {
+            const newPost = await this.dataService.createPost(readingData);
+            response.status(200).json(newPost);
+        } catch (error) {
+            console.log('eeee', error)
+
+            console.error(`Validation Error: ${error.message}`);
+            response.status(400).json({error: 'Invalid input data.'});
+        }
     }
 
     private deleteAllPosts = async (request: Request, response: Response, next: NextFunction)=> {
-        testArr = [];
-
+        await this.dataService.deleteData({});
         response.status(200);
     }
 
-    private deleteOnePost = async (request: Request, response: Response, next: NextFunction)=> {
+    private deletePostById = async (request: Request, response: Response, next: NextFunction)=> {
         const { id } = request.params;
-        const postId: number = parseInt(id);
+        await this.dataService.deleteData({_id: id});
+        response.sendStatus(200);
+    };
 
-        testArr.splice(postId,1);
-
-        response.status(200);
-    }
-
-    private getXPosts = async (request: Request, response: Response, next: NextFunction)=> {
+    private getXPosts = async (request: Request, response: Response, next: NextFunction) => {
         const { num } = request.params;
         const numberOfPosts: number = parseInt(num);
 
-        response.status(200).json(testArr.slice(0,numberOfPosts))
+        try {
+            const allPosts = await this.dataService.query({});
+            const limitedPosts = allPosts.slice(0, numberOfPosts);
+            response.status(200).json(limitedPosts);
+        } catch (error) {
+            console.error('Wystąpił błąd podczas pobierania postów:', error);
+            response.status(500).json({ error: 'Internal Server Error' });
+        }
     }
+
 }
 
 export default PostController;
