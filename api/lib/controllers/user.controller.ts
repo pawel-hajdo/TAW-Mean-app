5aa216f6c1ef97daafdf5c50e4485166e5685353
Paweł Hajdo <pawel.hajdo001@gmail.com>
2024-04-28 21:31:06 +0200

    added methods in userController

--- api/lib/controllers/user.controller.ts
+++ api/lib/controllers/user.controller.ts
@@ -18,11 +18,58 @@ class UserController implements Controller {
     }
 
     private initializeRoutes() {
-        // this.router.post(`${this.path}/create`, this.createNewOrUpdate);
-        // this.router.post(`${this.path}/auth`, this.authenticate);
-        // this.router.delete(`${this.path}/logout/:userId`,  this.removeHashSession);
+        this.router.post(`${this.path}/create`, this.createNewOrUpdate);
+        this.router.post(`${this.path}/auth`, this.authenticate);
+        this.router.delete(`${this.path}/logout/:userId`,  this.removeHashSession);
     }
 
+    private createNewOrUpdate = async (request: Request, response: Response, next: NextFunction) => {
+        const userData = request.body;
+
+        try {
+            const user = await this.userService.createNewOrUpdate(userData);
+            if(userData.password){
+                const hashedPassword = await this.passwordService.hashPassword(userData.password)
+                await this.passwordService.createOrUpdate({
+                    userId: user._id,
+                    password: hashedPassword,
+                })
+            }
+            response.status(200).json(user);
+        } catch (error) {
+            console.error(`Validation Error: ${error.message}`);
+            response.status(400).json({error: 'Bad request', value: error.message});
+        }
+    }
+
+    private authenticate = async (request: Request, response: Response, next: NextFunction) => {
+        const {login, password} = request.body;
+
+        try {
+            const user = await this.userService.getByEmailOrName(login);
+            if(!user){
+                response.status(401).json({error: 'Unauthorized'});
+            }
+            await this.passwordService.authorize(user.id, await this.passwordService.hashPassword(password));
+            const token = await this.tokenService.create(user);
+            response.status(200).json(this.tokenService.getToken(token));
+        } catch (error) {
+            console.error(`Validation error: ${error.message}`);
+            response.status(401).json({error: 'Unauthorized'});
+        }
+    }
+
+    private removeHashSession = async (request: Request, response: Response, next: NextFunction) => {
+        const {userId} = request.params;
+
+        try {
+            const result = await this.tokenService.remove(userId);
+            response.status(200).send(result);
+        } catch (error) {
+            console.error(`Validation Error: ${error.message}`);
+            response.status(401).json({error: 'Unauthorized'});
+        }
+    }
 }
 
 export default UserController;
--- api/lib/index.ts
+++ api/lib/index.ts
@@ -1,8 +1,10 @@
 import App from './app';
 import IndexController from "./controllers/index.controller";
 import PostController from "./controllers/post.controller";
+import UserController from "./controllers/user.controller";
 
 const app: App = new App([
+    new UserController(),
     new PostController(),
     new IndexController()
 ]);
