893e9ee54bfb3c7300d58f0662f8f79dfa3db1a8
Paweł Hajdo <pawel.hajdo001@gmail.com>
2024-05-30 19:04:00 +0200

    pipe for text blog items css

--- app/src/app/components/blog-item-text/blog-item-text.component.ts
+++ app/src/app/components/blog-item-text/blog-item-text.component.ts
@@ -1,9 +1,10 @@
 import {Component, Input} from '@angular/core';
+import {SummaryPipe} from "../../pipes/summary.pipe";
 
 @Component({
   selector: 'blog-item-text',
   standalone: true,
-  imports: [],
+  imports: [SummaryPipe],
   templateUrl: './blog-item-text.component.html',
   styleUrl: './blog-item-text.component.scss'
 })
