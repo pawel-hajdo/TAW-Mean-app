ab4149696ca55041867c395694cd9125b4ab9d5a
Paweł Hajdo <pawel.hajdo001@gmail.com>
2024-05-30 18:23:52 +0200

    first component

---
+++ app/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  public counter = 0;
+
+  add() {
+    this.counter++;
+  }
+
+  remove() {
+    this.counter--;
+  }
+}
