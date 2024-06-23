70fcf20cc5e9e3dcd692a89d5a618a80a746d828
Paweł Hajdo <pawel.hajdo001@gmail.com>
2024-06-23 16:35:28 +0200

    home page

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -2,14 +2,19 @@ import { Routes } from '@angular/router';
 import {BlogComponent} from "./components/blog/blog.component";
 import {BlogItemDetailsComponent} from "./components/blog-item-details/blog-item-details.component";
 import {BlogHomeComponent} from "./components/blog-home/blog-home.component";
+import {HomeComponent} from "./components/home/home.component";
 
 export const routes: Routes = [
   {
     path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'blog',
     component: BlogHomeComponent
   },
   {
-    path: 'blog/details/:id',
+    path: 'blog/detail/:id',
     component: BlogItemDetailsComponent
   }
 ];
--- app/src/app/shared/search-bar/search-bar.component.ts
+++ app/src/app/shared/search-bar/search-bar.component.ts
@@ -26,7 +26,7 @@ export class SearchBarComponent implements OnInit{
 
 
   sendFilter() {
-    this.router.navigate(['/'], {queryParams: {name:
+    this.router.navigate(['/blog'], {queryParams: {name:
           this.filterText?.toLowerCase()
       }});
     this.name.emit(this.filterText);
